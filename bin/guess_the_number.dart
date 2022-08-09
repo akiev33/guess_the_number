0eeb5589d5d31ae47c1d30762d454a35cd7fdcab
akiev33 <71421550+akiev33@users.noreply.github.com>
2022-08-09 21:17:17 +0600

    function 3

--- bin/guess_the_number.dart
+++ bin/guess_the_number.dart
@@ -5,12 +5,14 @@ import 'dart:math';
 
 void main() {
   print(
-      'Выберите режим игры: \n1) Человек отгадывает число \n2) Компьютер отгадывает число');
+      'Выберите режим игры: \n1) Человек отгадывает число \n2) Компьютер отгадывает число\n3) Соревнование с компьютером');
   int choice = int.parse(stdin.readLineSync()!);
   if (choice == 1) {
     random_computer();
   } else if (choice == 2) {
     random_user();
+  } else if (choice == 3) {
+    equal();
   }
 }
 
@@ -18,13 +20,13 @@ void random_computer() {
   var rng = Random();
   // random от 1 до 10
   int random_number = rng.nextInt(100) + 1;
-  int i = 0;
+  // int i = 0;
   // счетчик
   int count = 1;
   bool is_active_game = true;
 
   print('Я загадал число от 1 до 100');
-  
+
   do {
     int user_input = int.tryParse(stdin.readLineSync()!) ?? 105;
     if (user_input > 0 && user_input <= 100) {
@@ -49,7 +51,7 @@ void random_computer() {
 
 // среднее кол-во попыток для отгадывания числа 6
 void random_user() {
-  int i = 0;
+  // int i = 0;
   int t1 = 1;
   int t2 = 100;
   int count = 1;
@@ -76,3 +78,62 @@ void random_user() {
     }
   } while (is_active_game);
 }
+
+void equal() {
+  print(
+      'Я загадаю число от 1 до 100, и ты тоже должен загадать число от 1 до 100\nИ по очереди будем угадывать числа, кто первый отгадает, победит!');
+  //computer
+  int randomNumber = Random().nextInt(100) + 1;
+  // print(randomNumber);
+  int count1 = 1;
+  int count2 = 1;
+  bool isActiveGame = true;
+  print('Поехали! \nЯ загадал число от 1 до 100');
+  //user
+  int min = 1;
+  int max = 100;
+
+  do {
+    //user logic
+    int user_input = int.tryParse(stdin.readLineSync()!) ?? 105;
+    if (user_input > 0 && user_input <= 100) {
+      if (user_input == randomNumber) {
+        print('Молодец, ты угадал! Кол-во попыток $count1');
+        isActiveGame = false;
+      } else if (randomNumber > user_input) {
+        print('Число больше $user_input');
+        count1++;
+      } else if (randomNumber < user_input) {
+        print('Число меньше $user_input');
+        count1++;
+      }
+    } else if (user_input == 105) {
+      print('Ошибка!');
+    } else {
+      print('Вводи от 1 до 100');
+    }
+
+    // computer logic
+    if (isActiveGame == true) {
+      int x = (min + max) ~/ 2;
+      print(
+          'Твое число больше, меньше или равно $x ? \n1 - Больше  2 - Меньше  3 - Равно');
+      int user_choice = int.tryParse(stdin.readLineSync()!) ?? 105;
+      if (user_choice == 3) {
+        print('Я угадал. Спасибо за игру! Кол-во моих попыток $count2');
+        isActiveGame = false;
+      } else if (user_choice == 1) {
+        min = x;
+        count2++;
+      } else if (user_choice == 2) {
+        max = x;
+        count2++;
+      } else if (user_choice == 105) {
+        print('Пиши 1, 2 или 3');
+      } else if (user_choice <= 0 || user_choice > 3) {
+        print('Ошибка, у тебя тока 3 выбора (1, 2, 3)');
+      }
+    }
+  } while (isActiveGame);
+}
+
